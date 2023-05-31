bbfe4c84d1da0bb4b94c142e21759e3db10ab91f
Técnico em Informática 2022.2 <16127512022.2@senacrjedu.local>
2023-05-31 19:47:37 -0300

    aula 07

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { User } from '../Model/user';
+import { NOMEM } from 'dns';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,21 @@ import { Injectable } from '@angular/core';
 export class UserService {
 
   constructor() { }
+
+ private firestore: Firestore = inject(Firestore); // inject Cloud Firestore 
+ 
+ private userCollection = collection(this.firestore, 'users');
+
+ add(user: User) {
+  return addDoc(this.userCollection, <User>{
+    email: user.email,
+    nome: user.nome,
+    senha: user.senha,
+    telefone: user.telefone
+
+
+  })
+  
+ }
+
 }
